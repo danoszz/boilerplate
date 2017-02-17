26429bb27f655a0b9f0d33b606c7a241bce415e8
danoszz <danoszz@gmail.com>
2017-02-17 15:27:19 +0100

    Changed author (no hard feelings Hossein)

--- app/assets/js/scripts.js
+++ app/assets/js/scripts.js
@@ -1,9 +1,9 @@
 /*!
- * fastshell
- * Fiercely quick and opinionated front-ends
- * https://HosseinKarami.github.io/fastshell
- * @author Hossein Karami
- * @version 1.0.5
+ * boilerplate
+ * Quick way to setup your Gulp + SASS front-end environment
+ * https://github.com/danoszz/boilerplate
+ * @author Daan van der Zwaag
+ * @version 1.0
  * Copyright 2017. MIT licensed.
  */
 (function ($, window, document, undefined) {
