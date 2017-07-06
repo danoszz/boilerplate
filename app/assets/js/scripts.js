fe717a9854526e4198bd61855ee729c913ac9b47
danoszz <danoszz@gmail.com>
2017-07-06 23:15:48 +0200

    Big design update + imageHover

--- app/assets/js/scripts.js
+++ app/assets/js/scripts.js
@@ -6,11 +6,40 @@
  * @version 1.0.1
  * Copyright 2017. MIT licensed.
  */
+/*!
+ * internsite
+ * Internsite
+ * https://internsite.dvdz.design
+ * @author Daan van der Zwaag
+ * @version 1.0.0
+ * Copyright 2016. MIT licensed.
+ */
 (function($, window, document, undefined) {
 
   'use strict';
 
   $(function() {
+
+    // Little big details
+    var messagesGone = ['Don\'t ditch me', 'Come back please', 'Ignoring me?', 'I\'m gonna cry', 'Getting drunk brb'];
+    var messagesLink = ['Nice one', 'Check it out', 'Personal fav', 'Random', 'Ayyy lmao'];
+    var original;
+
+    $(window).focus(function() {
+      if (original) {
+        document.title = original;
+      }
+    }).blur(function() {
+      var title = $('title').text();
+      var choosenMessage = messagesGone[Math.floor(Math.random() * messagesGone.length)];
+      if (title !== choosenMessage) {
+        original = title;
+      }
+      document.title = choosenMessage;
+    });
+
+    // HoverImage
+
 
   });
 
